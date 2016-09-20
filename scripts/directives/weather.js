0dd26358e89ac2722991b83975b828a81f3d2011
mrogach2350 <m.rogach@gmail.com>
2016-09-20 11:44:42 -0700

    adds controller to directive

--- scripts/directives/weather.js
+++ scripts/directives/weather.js
@@ -7,8 +7,21 @@ angular.modlule('weatherApp')
         location: '@'
       },
       restrict: 'EA',
-      templateUrl: 
-      replace: true
+      templateUrl:
+      replace: true,
+      controllerAs: weatherCtrl,
+      controller: ['$http', function($http){
+        var vm = this;
+
+        vm.apiKey = '47d3444e8706e36ec0847be0300c39e0';
+        $http({
+            method: 'GET',
+            url: 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=' + vm.apiKey;
+        }).then(function successCallback(json){
+          vm.weather = json.
+          console.log(vm.weather);
+        });
+      }];
     };
     return directive;
   }
