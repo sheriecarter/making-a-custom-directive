c694d2332483396a8044269d15141fe3fc4a23c7
mrogach2350 <m.rogach@gmail.com>
2016-09-20 12:16:46 -0700

    latest version

--- scripts/directives/weather.js
+++ scripts/directives/weather.js
@@ -1,4 +1,5 @@
-angular.modlule('weatherApp')
+angular
+  .module('weatherApp')
   .directive('weatherFinder', weatherFinder)
 
   function weatherFinder(){
@@ -7,21 +8,22 @@ angular.modlule('weatherApp')
         location: '@'
       },
       restrict: 'EA',
-      templateUrl:
+      // templateUrl:
       replace: true,
-      controllerAs: weatherCtrl,
+      controllerAs: 'weatherCtrl',
       controller: ['$http', function($http){
         var vm = this;
-
-        vm.apiKey = '47d3444e8706e36ec0847be0300c39e0';
-        $http({
-            method: 'GET',
-            url: 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=' + vm.apiKey;
-        }).then(function successCallback(json){
-          vm.weather = json.
-          console.log(vm.weather);
-        });
-      }];
+        var url="http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q=";
+        var apikey = "&appid=" + '47d3444e8706e36ec0847be0300c39e0';
+        vm.getWeather = function(){
+          $http({
+            method: 'JSONP',
+            url: url + 524901 + apikey
+          }).success(function(data){
+            vm.weather = data;
+          });
+        }
+      }]
     };
     return directive;
   }
